2b6bc41bde359046a795f88f6aab70c787e6843e
Adi <asanand1297@gmail.com>
2025-05-23 20:45:15 +0530

    pushed .env

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,27 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
   })
 );
+
 app.use(express.json());
 app.use(rateLimter);
 
 app.use("/api/notes", notesRoutes);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 
 connectDB().then(() => {
   app.listen(PORT, () => {
